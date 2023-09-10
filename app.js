189591a5b6a496da23b52b956ffc917f4f964f0f
Kretov222 <Kretov222@yandex.ru>
2023-09-10 19:39:07 +0300

    Updated2

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// const filterString = (string, symbol) =>{
-//   let result = '';
-//     for(let i = string.length -1 ; i>=0; i--){
-//      const currentChar = string[i];
-//       if(currentChar!== symbol){
-//       result= currentChar+ result}; 
-//     }
-//     return result;
-//   }
+const filterString = (string, symbol) =>{
+  let result = '';
+    for(let i = string.length -1 ; i>=0; i--){
+     const currentChar = string[i];
+      if(currentChar!== symbol){
+      result= currentChar+ result}; 
+    }
+    return result;
+  }
 
-// console.log(filterString('Hello', 'H'))
+console.log(filterString('Hello', 'H'))
 
 
 
@@ -25,20 +25,19 @@ function hasChar(string, char){
 }
 console.log(hasChar);
 
-//const text = 'I never look back';
-//const makeItFunny = (text, num) =>{
-  //let result = '';
-  //for(i=0; i< text.length; i++){
-   // if ((i+1)% num == 0){
-      //console.log(i)
-      //result += text[i].toUpperCase()
-    //} else result += text[i]
-  //}
-  //return result;
-//}
-//console.log(makeItFunny(text,3));
+const text = 'I never look back';
+const makeItFunny = (text, num) =>{
+  let result = '';
+  for(i=0; i< text.length; i++){
+   if ((i+1)% num == 0){
+      console.log(i)
+      result += text[i].toUpperCase()
+    } else result += text[i]
+  }
+  return result;
+}
+console.log(makeItFunny(text,3));
 
 
 
 
-// проверка 1234
